e3e85e6293555ee0781f6bf53344aaa3f8473307
skillforce <skillforce@mail.ru>
2023-02-19 17:21:25 +0300

    webpack bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 export const buildPlugins = ({ paths, isDev }:BuildOptions):webpack.WebpackPluginInstance[] => {
     const plugins = [
@@ -18,6 +19,7 @@ export const buildPlugins = ({ paths, isDev }:BuildOptions):webpack.WebpackPlugi
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
+        new BundleAnalyzerPlugin({ openAnalyzer: false }),
 
     ];
     if (isDev) {
