de539c1ef2eff7b3e4d99e6dcf7fbf3cd8907f92
skillforce <skillforce@mail.ru>
2023-02-24 21:37:26 +0300

    CI feature (all test autorun config)

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -9,6 +9,5 @@ describe('Button', () => {
     test('with clear theme', () => {
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
-        screen.debug();
     });
 });
