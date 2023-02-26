c0c9fa13146f06f9c9e0f05508e7621a9e71f0a8
skillforce <skillforce@mail.ru>
2023-02-26 19:34:31 +0300

    reg-cli (comfortable UI for loki using)

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -4,10 +4,20 @@ import { I18nextProvider } from 'react-i18next';
 import i18nForTest from 'shared/config/i18n/i18nForTest';
 import { MemoryRouter } from 'react-router-dom';
 
-export const componentRender = (component:ReactNode) => render(
-    <MemoryRouter>
-        <I18nextProvider i18n={i18nForTest}>
-            {component}
-        </I18nextProvider>
-    </MemoryRouter>,
-);
+export interface componentRenderOptions{
+    route?:string
+}
+
+export const componentRender = (component:ReactNode, options:componentRenderOptions = {}) => {
+    const {
+        route,
+    } = options;
+
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18nForTest}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>,
+    );
+};
