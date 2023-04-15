cb2278d3a0b56c399286eacb21385c3e730fb13b
skillforce <skillforce@mail.ru>
2023-04-15 18:44:06 +0300

    ts fixes last one

--- src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,12 +1,6 @@
-import axios from 'axios';
-import { Dispatch } from '@reduxjs/toolkit';
-import { StateSchema } from 'app/providers/StoreProvider';
 import { userActions } from 'entity/User';
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 import { loginByUsername } from './loginByUsername';
-
-jest.mock('axios');
-const mockedAxios = jest.mocked(axios, true);
 
 describe('loginByUsername.test', () => {
     // let dispatch: Dispatch;
@@ -39,24 +33,24 @@ describe('loginByUsername.test', () => {
     // });
     test('test loginByUserName thunk success', async () => {
         const finallyUserData = { username: 'Denis', id: '1' };
-        mockedAxios.post.mockReturnValue(Promise.resolve({ data: finallyUserData }));
         const thunk = new TestAsyncThunk(loginByUsername);
+        thunk.api.post.mockReturnValue(Promise.resolve({ data: finallyUserData }));
         const result = await thunk.callThunk({ username: 'Denis', password: '12345' });
 
-        // expect(result.payload).toEqual(finallyUserData);
-        // expect(mockedAxios.post).toHaveBeenCalled();
-        // expect(result.meta.requestStatus).toBe('fulfilled');
-        // expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(finallyUserData));
+        expect(result.payload).toEqual(finallyUserData);
+        expect(thunk.api.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(finallyUserData));
     });
     test('test loginByUserName thunk error', async () => {
         const errorResponse = { status: 403 };
-        mockedAxios.post.mockReturnValue(Promise.resolve(errorResponse));
         const thunk = new TestAsyncThunk(loginByUsername);
+        thunk.api.post.mockReturnValue(Promise.resolve(errorResponse));
         const result = await thunk.callThunk({ username: 'Denis', password: '12233' });
-        //
-        // expect(thunk.dispatch).toHaveBeenCalledTimes(2);
-        // expect(mockedAxios.post).toHaveBeenCalled();
-        // expect(result.meta.requestStatus).toBe('rejected');
-        // expect(result.payload).toBe('error');
+
+        expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+        expect(thunk.api.post).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('rejected');
+        expect(result.payload).toBe('error');
     });
 });
--- src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
+++ src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -1,8 +1,12 @@
 import { StateSchema } from 'app/providers/StoreProvider';
 import { AsyncThunkAction, Dispatch } from '@reduxjs/toolkit';
+import axios, { AxiosStatic } from 'axios';
+
+jest.mock('axios');
+const mockedAxios = jest.mocked(axios, true);
 
 type ActionCreatorType<Returned, ThunkArg, RejectedValue>
-    = (arg:ThunkArg) => AsyncThunkAction<Returned, ThunkArg, {rejectValue: RejectedValue}>
+    = () => AsyncThunkAction<Returned, ThunkArg, {rejectValue: RejectedValue}>
 export class TestAsyncThunk<Returned, ThunkArg, RejectedValue> {
     dispatch:Dispatch;
 
@@ -10,15 +14,29 @@ export class TestAsyncThunk<Returned, ThunkArg, RejectedValue> {
 
     actionCreator:ActionCreatorType<Returned, ThunkArg, RejectedValue>;
 
+    api: jest.MockedFunctionDeep<AxiosStatic>;
+
+    navigate:jest.MockedFn<any>;
+
     constructor(actionCreator:ActionCreatorType<Returned, ThunkArg, RejectedValue>) {
         this.actionCreator = actionCreator;
         this.dispatch = jest.fn();
         this.getState = jest.fn();
+
+        this.navigate = jest.fn();
+        this.api = mockedAxios;
     }
 
     async callThunk(arg:ThunkArg) {
         const action = this.actionCreator(arg);
-        const result = await action(this.dispatch, this.getState, undefined);
+        const result = await action(
+            this.dispatch,
+            this.getState,
+            {
+                api: this.api,
+                navigate: this.navigate,
+            },
+        );
         return result;
     }
 }
