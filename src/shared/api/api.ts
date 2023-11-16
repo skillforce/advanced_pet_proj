7a3d72225c97021662e8f7a76c5ae693c7cb0ab9
skillforce <skillforce@mail.ru>
2023-11-16 17:46:10 +0300

    pre production deploy config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -7,7 +7,8 @@ export const $api = axios.create({
 
 $api.interceptors.request.use((config) => {
     if (config.headers) {
-        config.headers.Authorization = localStorage.getItem(AUTH_USER_LOCAL_STORAGE) || '';
+        // config.headers.Authorization = localStorage.getItem(AUTH_USER_LOCAL_STORAGE) || '';
+        config.headers.Authorization = 'Bearer t5rGrxfh1K74QW4IAppl21Qf';
     }
     return config;
 });
